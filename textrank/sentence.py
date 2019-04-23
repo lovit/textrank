2047a6c06905e339bf244676cc18b5e14b60d687
lovit <soy.lovit@gmail.com>
2019-04-23 17:27:40 +0900

    Cosine sentence similarity

--- textrank/sentence.py
+++ textrank/sentence.py
@@ -1,4 +1,6 @@
+from collections import Counter
 import math
+
 
 def textrank_sent_sim(s1, s2):
     """
@@ -19,3 +21,27 @@ def textrank_sent_sim(s1, s2):
     common = len(set(s1).intersection(set(s2)))
     base = math.log(n1) + math.log(n2)
     return common / base
+
+def cosine_sent_sim(s1, s2):
+    """
+    Arguments
+    ---------
+    s1, s2 : list of str
+        Tokenized sentences
+
+    Returns
+    -------
+    Sentence similarity : float
+        Non-negative number
+    """
+    if (not s1) or (not s2):
+        return 0
+
+    s1 = Counter(s1)
+    s2 = Counter(s2)
+    norm1 = math.sqrt(sum(v ** 2 for v in s1.values()))
+    norm2 = math.sqrt(sum(v ** 2 for v in s2.values()))
+    prod = 0
+    for k, v in s1.items():
+        prod += v * s2.get(k, 0)
+    return prod / (norm1 * norm2)
