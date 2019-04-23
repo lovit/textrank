91a949229e06beb272d5abb5472c8445409c008a
lovit <soy.lovit@gmail.com>
2019-04-23 23:26:49 +0900

    Minor

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import textrank
+
+setup(
+    name=textrank.__name__,
+    version=textrank.__version__,
+    url='https://github.com/lovit/textrank/',
+    author=textrank.__author__,
+    author_email='soy.lovit@gmail.com',
+    description='TextRank based Summarizer (Keyword and key-sentence extractor)',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    setup_requires=[]
+)
--- textrank/__init__.py
+++ textrank/__init__.py
@@ -1,6 +1,6 @@
 __name__ = 'textrank'
 __author__ = 'lovit'
-__version__ == '0.1.0'
+__version__ = '0.1.0'
 
 from .summarizer import KeywordSummarizer
 from .summarizer import KeysentenceSummarizer
