f772277f893398e4f6dc8346f139345a86449076
lovit <soy.lovit@gmail.com>
2019-04-23 18:13:14 +0900

    Word cooccurrence matrix

---
+++ textrank/word.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from scipy.sparse import csr_matrix
+
+
+def cooccurrence(tokens, vocab_to_idx, min_cooccurrence=2, window=2):
+    counter = defaultdict(int)
+    for tokens_i in tokens:
+        vocabs = [vocab_to_idx[w] for w in tokens_i if w in vocab_to_idx]
+        n = len(vocabs)
+        for i, v in enumerate(vocabs):
+            if window <= 0:
+                b, e = 0, n
+            else:
+                b = max(0, i - window)
+                e = min(i + window, n)
+            for j in range(b, e):
+                if i == j:
+                    continue
+                counter[(v, vocabs[j])] += 1
+                counter[(vocabs[j], v)] += 1
+    counter = {k:v for k,v in counter.items() if v >= min_cooccurrence}
+    n_vocabs = len(vocab_to_idx)
+    return dict_to_mat(counter, n_vocabs, n_vocabs)
+
+def dict_to_mat(d, n_rows, n_cols):
+    rows, cols, data = [], [], []
+    for (i, j), v in d.items():
+        rows.append(i)
+        cols.append(j)
+        data.append(v)
+    return csr_matrix((data, (rows, cols)), shape=(n_rows, n_cols))
