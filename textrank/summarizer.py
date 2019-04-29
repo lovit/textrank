6787ddc7826f543737c91a78fafe2d54484edd7e
lovit <soy.lovit@gmail.com>
2019-04-29 20:43:21 +0900

    Docstring for KeysentenceSummarizer

--- textrank/summarizer.py
+++ textrank/summarizer.py
@@ -42,6 +42,32 @@ class KeywordSummarizer:
 
 
 class KeysentenceSummarizer:
+    """
+    Arguments
+    ---------
+    sents : list of str
+        Sentence list
+    tokenize : callable
+        Tokenize function: tokenize(str) = list of str
+    min_count : int
+        Minumum frequency of words will be used to construct sentence graph
+    min_sim : float
+        Minimum similarity between sentences in sentence graph
+    similarity : callable or str
+        similarity(s1, s2) returns float
+        s1 and s2 are list of str.
+        available similarity = [callable, 'cosine', 'textrank']
+    vocab_to_idx : dict or None
+        Vocabulary to index mapper
+    df : float
+        PageRank damping factor
+    max_iter : int
+        Number of PageRank iterations
+    bias : None or numpy.ndarray
+        PageRank bias term
+    verbose : Boolean
+        If True, it shows training progress
+    """
     def __init__(self, sents=None, tokenize=None, min_count=2,
         min_sim=0.3, similarity=None, vocab_to_idx=None,
         df=0.85, max_iter=30, bias=None, verbose=False):
@@ -60,6 +86,16 @@ class KeysentenceSummarizer:
             self.train_textrank(sents)
 
     def train_textrank(self, sents):
+        """
+        Arguments
+        ---------
+        sents : list of str
+            Sentence list
+
+        Returns
+        -------
+        None
+        """
         g = sent_graph(sents, self.tokenize, self.min_count,
             self.min_sim, self.similarity, self.vocab_to_idx, self.verbose)
         self.R = pagerank(g, self.df, self.max_iter, self.bias).reshape(-1)
@@ -67,6 +103,26 @@ class KeysentenceSummarizer:
             print('trained TextRank. n sentences = {}'.format(self.R.shape[0]))
 
     def summarize(self, sents, topk=30):
+        """
+        Arguments
+        ---------
+        sents : list of str
+            Sentence list
+        topk : int
+            Number of key-sentences to be selected.
+
+        Returns
+        -------
+        keysents : list of tuple
+            Each tuple stands for (sentence index, rank, sentence)
+
+        Usage
+        -----
+            >>> from textrank import KeysentenceSummarizer
+
+            >>> summarizer = KeysentenceSummarizer(tokenize = tokenizer, min_sim = 0.5)
+            >>> keysents = summarizer.summarize(texts, topk=30)
+        """
         if not hasattr(self, 'R'):
             self.train_textrank(sents)
         elif len(sents) != self.R.shape[0]:
