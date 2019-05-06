728398a888c6aa8ec9a861e163fbfd89ee653684
lovit <soy.lovit@gmail.com>
2019-05-07 00:01:32 +0900

    Reimplemented textrank sentence similarity with numpy

--- textrank/sentence.py
+++ textrank/sentence.py
@@ -47,7 +47,7 @@ def sent_graph(sents, tokenize=None, min_count=2, min_sim=0.3,
     if similarity == 'cosine':
         x = numpy_cosine_similarity_matrix(x, min_sim, verbose, batch_size=1000)
     else:
-        raise NotImplemented('Will be implemented TextRank similarity with numpy')
+        x = numpy_textrank_similarity_matrix(x, min_sim, verbose, batch_size=1000)
     return x
 
 def vectorize_sents(sents, tokenize, vocab_to_idx):
@@ -76,10 +76,55 @@ def numpy_cosine_similarity_matrix(x, min_sim=0.3, verbose=True, batch_size=1000
         data = psim[rows, cols]
         mat.append(csr_matrix((data, (rows, cols)), shape=(e-b, n_rows)))
         if verbose:
-            print('\rcalculating sentence similarity {} / {}'.format(b, n_rows), end='')
+            print('\rcalculating cosine sentence similarity {} / {}'.format(b, n_rows), end='')
     mat = sp.sparse.vstack(mat)
     if verbose:
-        print('\rcalculating sentence similarity was done from {} sents'.format(n_rows))
+        print('\rcalculating cosine sentence similarity was done with {} sents'.format(n_rows))
+    return mat
+
+def numpy_textrank_similarity_matrix(x, min_sim=0.3, verbose=True, min_length=1, batch_size=1000):
+    n_rows, n_cols = x.shape
+
+    # Boolean matrix
+    rows, cols = x.nonzero()
+    data = np.ones(rows.shape[0])
+    z = csr_matrix((data, (rows, cols)), shape=(n_rows, n_cols))
+
+    # Inverse sentence length
+    size = np.asarray(z.sum(axis=1)).reshape(-1)
+    size[np.where(size <= min_length)] = 10000
+    size = np.log(size)
+
+    mat = []
+    for bidx in range(math.ceil(n_rows / batch_size)):
+
+        # slicing
+        b = int(bidx * batch_size)
+        e = min(n_rows, int((bidx+1) * batch_size))
+
+        # dot product
+        inner = z[b:e,:] * z.transpose()
+
+        # sentence len[i,j] = size[i] + size[j]
+        norm = size[b:e].reshape(-1,1) + size.reshape(1,-1)
+        norm = norm ** (-1)
+        norm[np.where(norm == np.inf)] = 0
+
+        # normalize
+        sim = inner.multiply(norm).tocsr()
+        rows, cols = (sim >= min_sim).nonzero()
+        data = np.asarray(sim[rows, cols]).reshape(-1)
+
+        # append
+        mat.append(csr_matrix((data, (rows, cols)), shape=(e-b, n_rows)))
+
+        if verbose:
+            print('\rcalculating cosine sentence similarity {} / {}'.format(b, n_rows), end='')
+
+    mat = sp.sparse.vstack(mat)
+    if verbose:
+        print('\rcalculating cosine sentence similarity was done with {} sents'.format(n_rows))
+
     return mat
 
 def graph_with_python_sim(tokens, verbose, similarity, min_sim):
